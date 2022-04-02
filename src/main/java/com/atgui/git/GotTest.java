4cf7c86704ba5d22293a21fd5226e9be197276ac
root <root@163.com>
2022-04-02 19:45:31 +0800

    g02

--- src/main/java/com/atgui/git/GotTest.java
+++ src/main/java/com/atgui/git/GotTest.java
@@ -3,5 +3,6 @@ package com.atgui.git;
 public class GotTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git11");
     }
 }
